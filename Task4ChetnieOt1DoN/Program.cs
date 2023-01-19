d9e4e8f18b32eac849655789a9cb874621fdf556
Kate V <katsiarynait@gmail.com>
2023-01-20 00:34:05 +0300

    с учётом возможности ввода в том числе отрицательных чисел

--- Task4ChetnieOt1DoN/Program.cs
+++ Task4ChetnieOt1DoN/Program.cs
@@ -3,7 +3,9 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8 */
 
-Console.WriteLine("Введите число");
+/*решение без учёта ввода пользователем отрицательных чисел:
+
+ Console.WriteLine("Введите число");
 int n = Convert.ToInt32(Console.ReadLine());
 int numberToWrite = 1;
 while (numberToWrite < n + 1)
@@ -13,4 +15,38 @@ while (numberToWrite < n + 1)
         System.Console.WriteLine(numberToWrite);
     }
     numberToWrite++;
+} */
+
+/*А это решение с учётом ввода отрицательных чисел или 0 пользователем: 
+*/
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int numberToWrite = 1;
+
+if (n > numberToWrite)
+{
+    while (numberToWrite < n + 1)
+    {
+        if (numberToWrite % 2 == 0)
+        {
+            System.Console.WriteLine(numberToWrite);
+        }
+        numberToWrite++;
+    }
 }
+else
+    while (numberToWrite > n - 1)
+    {
+        if (numberToWrite % 2 == 0)
+        {
+            System.Console.WriteLine(numberToWrite);
+        }
+        numberToWrite = numberToWrite - 1;
+    }
+
+    /* Владимир, подскажите, пожалуйста, можно ли это (последний вариант) 
+    решить как-то ещё короче, чем у меня (в зависимости от того, меньше или больше 1 число N,
+     которое вводит пользователь)? 
+    Спасибо за ответ!
+    */
